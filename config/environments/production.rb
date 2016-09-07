4dccf8514ab98e06047deb31bedfb982953e2160
wenwei63029869 <wwen1030@gmail.com>
2016-09-07 17:16:10 -0500

    done setting up production redis path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.web_socket_server_url = "wss://w-message.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://w-message.herokuapp.com', 'http://w-message.herokuapp.com']
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
